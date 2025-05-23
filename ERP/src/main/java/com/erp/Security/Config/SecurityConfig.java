104469f843408411aec052b01d2611b19bf9a6e9
Vishal Kholakiya <vishalkholakiya369@gmail.com>
2025-05-23 13:24:49 +0530

    created securityConfig file

--- ERP/src/main/java/com/erp/Security/Config/SecurityConfig.java
+++ ERP/src/main/java/com/erp/Security/Config/SecurityConfig.java
@@ -1,12 +1,32 @@
-package com.erp.Security;
+package com.erp.Security.Config;
 
+import com.erp.Config.AppEnv;
+import com.erp.Security.Filter.AuthFilter;
+import com.erp.Security.Filter.RefreshAuthFilter;
+import com.erp.Security.JWT.JWTService;
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@AllArgsConstructor
 public class SecurityConfig {
+
+    private final AppEnv env;
+    private final JWTService jwtService;
 
     @Bean
     PasswordEncoder passwordEncoder() {
@@ -14,5 +34,64 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    AuthenticationManager authenticationManger(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
 
+    @Bean
+    @Order(3)
+    SecurityFilterChain publicSecurityFilterChain(HttpSecurity http) throws Exception {
+        String baseUrl = env.getBaseUrl();
+        return http.csrf(csrf -> csrf.disable())
+                .securityMatchers(match ->
+                        match.requestMatchers(baseUrl + "/auth/register/**", baseUrl + "/login" ))//basically used to configure filter chain to accept request made to a specific pattern default ="/**"
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(baseUrl + "/auth/register/**", baseUrl + "/login")
+                        // .requestMatchers(baseUrl+"/restaurant_register/{userId}").hasAuthority("ADMIN")
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                // .formLogin(Customizer.withDefaults())
+                .addFilterBefore(new AuthFilter(jwtService), UsernamePasswordAuthenticationFilter.class)
+                .build();
+
+    }
+
+    @Bean
+    @Order(2)
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        String baseUrl = env.getBaseUrl();
+
+        return http.csrf(csrf -> csrf.disable())
+                .securityMatchers(match ->
+                        match.requestMatchers( baseUrl + "/root/**" ))//basically used to configure filter chain to accept request made to a specific pattern default ="/**"
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(baseUrl + "/root/**").hasAnyAuthority("ROOT")
+                        // .requestMatchers(baseUrl+"/restaurant_register/{userId}").hasAuthority("ADMIN")
+                        .anyRequest()
+                        .authenticated())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                // .formLogin(Customizer.withDefaults())
+                .addFilterBefore(new AuthFilter(jwtService), UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+    @Bean
+    @Order(1)
+    SecurityFilterChain refreshSecurityFilterChain(HttpSecurity http) throws Exception {
+        String baseUrl = env.getBaseUrl();
+
+        return http.csrf(csrf -> csrf.disable())
+                .securityMatchers(match ->
+                        match.requestMatchers(baseUrl + "/refresh-login/**"))
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest()
+                        .authenticated())
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(new RefreshAuthFilter(jwtService), UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
 }
