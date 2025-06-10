b3ec84cbcdd5f2ef92633383c7573c8da36ad9bf
qareerwale <qareerwale369@gmail.com>
2025-06-10 11:25:05 +0530

    had to change some security config for sync with frontend , but shwoing the cros policy error

--- ERP/src/main/java/com/erp/Security/Filter/AuthFilter.java
+++ ERP/src/main/java/com/erp/Security/Filter/AuthFilter.java
@@ -15,6 +15,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
+
 import java.io.IOException;
 
 @AllArgsConstructor
